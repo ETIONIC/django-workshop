6385f4939e74f248b2a27a981843e9d6ecf0bed5
Gabriel O. Masi <masigabriel@gmail.com>
2022-05-18 03:17:10 -0300

    IMPROVE: Admin fields/filters

--- musicians/admin.py
+++ musicians/admin.py
@@ -1,7 +1,33 @@
 from django.contrib import admin
 from musicians.models import Musician, Musicband, Album
+from django.utils.html import format_html
 
-admin.site.register(Musician)
-admin.site.register(Musicband)
+@admin.register(Musician)
+class MusicianAdmin(admin.ModelAdmin):
+    fields = (('first_name', 'last_name'), 'instrument')
+    list_display = ('first_name', 'last_name', 'instrument', 'colored_name', 'plays_guitar')
+    search_fields = ['first_name', 'last_name', 'instrument']
+    list_filter = ('instrument',)
+
+    @admin.display(
+        ordering='first_name',
+        description='Full name in red',
+    )
+    def colored_name(self, obj):
+        return format_html(
+            '<span style="color: red;">{}</span>',
+            obj.name,
+        )
+
+    @admin.display(boolean=True)
+    def plays_guitar(self, obj):
+        return 'Guitar' == obj.instrument
+
+@admin.register(Musicband)
+class MusicbandAdmin(admin.ModelAdmin):
+    exclude = ('members',)
+    list_filter = ('members',)
+
+
 admin.site.register(Album)
 # Register your models here.
