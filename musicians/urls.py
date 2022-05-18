99e927ed735d720c7f12d6f4c7952306f9d2f78f
Gabriel O. Masi <masigabriel@gmail.com>
2022-05-18 04:18:54 -0300

    IMPROVE: Musicians Endpoints with ViewSet Class

--- musicians/urls.py
+++ musicians/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from musicians import views
 
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'musicians', views.MusicianViewSet,basename="musicians")
+
+# The API URLs are now determined automatically by the router.
 urlpatterns = [
-    path('musicians/', views.MusicianList.as_view()),
-    path('musicians/<int:pk>/', views.MusicianDetail.as_view()),
+    path('', include(router.urls)),
 ]
--- musicians/views.py
+++ musicians/views.py
@@ -1,53 +1,41 @@
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import permissions
+from rest_framework import viewsets
 from musicians.models import Musician
 from musicians.serializers import MusicianSerializer
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
 
-
-class MusicianList(APIView):
+class MusicianViewSet(viewsets.ModelViewSet):
     """
-    List all musicians, or create a new musician.
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
     """
-    def get(self, request, format=None):
-        musicians = Musician.objects.all()
-        serializer = MusicianSerializer(musicians, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = MusicianSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    queryset = Musician.objects.all()
+    serializer_class = MusicianSerializer
 
 
 
-class MusicianDetail(APIView):
-    """
-    Retrieve, update or delete a musician instance.
-    """
-    def get_object(self, pk):
-        try:
-            return Musician.objects.get(pk=pk)
-        except Musician.DoesNotExist:
-            raise Http404
 
-    def get(self, request, pk, format=None):
-        musician = self.get_object(pk)
-        serializer = MusicianSerializer(musician)
-        return Response(serializer.data)
 
-    def put(self, request, pk, format=None):
-        musician = self.get_object(pk)
-        serializer = MusicianSerializer(musician, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, pk, format=None):
-        musician = self.get_object(pk)
-        musician.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+
+'''
+class MusicianDetail(mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    generics.GenericAPIView):
+    queryset = Musician.objects.all()
+    serializer_class = MusicianSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+'''
