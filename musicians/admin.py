afe614114e1b59cda596a4d74208b978316cd92c
Gabriel O. Masi <masigabriel@gmail.com>
2022-05-18 02:45:47 -0300

    NEW: Admin

--- musicians/admin.py
+++ musicians/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from musicians.models import Musician, Musicband, Album
 
+admin.site.register(Musician)
+admin.site.register(Musicband)
+admin.site.register(Album)
 # Register your models here.
